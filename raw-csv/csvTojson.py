8c937d8e515558c5d4f2a0db05eb96fc0f0f2ba1
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-03-22 23:52:09 +0700

    short.json :)

--- raw-csv/csvTojson.py
+++ raw-csv/csvTojson.py
@@ -1,12 +1,24 @@
+def printDic():
+    tmp = str(dic)
+    tmp = tmp.replace("\'","\"")
+    tmp = tmp.replace(",",",\n")
+    print(tmp)
 fileName = "GenEdList - "   #1.csv
 group = 5
 tblHeader = 2
+dic = {}        #For finding the group of subject
+longDic = {}    #For detail the subject
 for i in range(1,group+1):
     f = open(fileName+str(i)+".csv")
     header = tblHeader
     #print(f.read())
-    for line in f:
+    for _ in f:
         if header > 0:
             header -= 1
             continue
-        print(i,line)
+        #print(i,line)
+        line = _.split(",")
+        while len(line[0]) < 8:     #Add 0 for the missing one
+            line[0] = "0"+line[0]
+        dic[line[0]] = str(i)
+printDic()
