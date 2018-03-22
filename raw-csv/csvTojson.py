e32d6ef8b3f01155727acb14427fa6c96c21ed0b
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-03-22 23:36:59 +0700

    look good tblHeader cutted

--- raw-csv/csvTojson.py
+++ raw-csv/csvTojson.py
@@ -1,7 +1,12 @@
 fileName = "GenEdList - "   #1.csv
 group = 5
+tblHeader = 2
 for i in range(1,group+1):
     f = open(fileName+str(i)+".csv")
+    header = tblHeader
     #print(f.read())
     for line in f:
+        if header > 0:
+            header -= 1
+            continue
         print(i,line)
