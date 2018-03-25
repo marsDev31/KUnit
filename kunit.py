77dbfd18b3cd3df95bd8d0ec5c7e0452cb969082
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-03-26 00:17:03 +0700

    add print_list() for debuging

--- kunit.py
+++ kunit.py
@@ -1,3 +1,4 @@
+CONFIG = eval(open("config.kunit.json").read())
 REF = eval(open("short.json").read())
 LONGREF = eval(open("long.json").read())
 tbl = [[0,0,0,0,0,0],[],[],[],[],[]]  #Ex [[5,2,0,0,3,0],["01175161","01173151"],[],[],["01999021"],[]] -> [[sumOfCredit,g1's credit,...],[g1],[g2],...]
@@ -14,7 +15,25 @@ def add(table,subject):
     else:
         print(f"{name} [{subject}] already added")
     return table
+
 def remove(table,subject):
     #...
     return table
 
+def print_list(table):
+    #for console debug
+    nog = CONFIG["NumberOfGroup"]
+    groupName = open("NameOfGroup.txt").read().split("\n")
+    print("Sum of all credit =",table[0][0])
+    for i in range(1,nog+1):
+        print()
+        if table[0][i] == 0:
+            print("No subject in",groupName[i-1])
+            continue
+        print(f"You have {table[0][i]} credit in {groupName[i-1]} group")
+        for sid in table[i]:    #Subject ID
+            name = LONGREF[sid][1]
+            credit = LONGREF[sid][3]
+            longCredit = LONGREF[sid][4]
+            print(f"{sid} {name:40} {credit} {longCredit}")
+            
