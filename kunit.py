86ccb221e75c78c6db65f9bd3e256bce28fbdeda
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-03-28 17:20:41 +0700

    adding is working

--- kunit.py
+++ kunit.py
@@ -3,10 +3,10 @@ REF = eval(open("short.json").read())
 LONGREF = eval(open("long.json").read())
 tbl = [[0,0,0,0,0,0],[],[],[],[],[]]  #Ex [[5,2,0,0,3,0],["01175161","01173151"],[],[],["01999021"],[]] -> [[sumOfCredit,g1's credit,...],[g1],[g2],...]
 def add(table,subject):
+    subject = str(subject)
     group = int(REF[subject][0])
     name = REF[subject][1]
     credit = int(REF[subject][2])
-
     if subject not in table[group]:
         table[0][0] += credit
         table[0][group] += credit
--- portal.py
+++ portal.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def hello_world():
     return "I'm WORKING"
 
+#inp = [[5,3,0,2,0,0],['01999012'],[],['01999111'],[],[]]a01999021
+@app.route('/add/<inp>')
+def add_to_table(inp):
+    table = inp[:inp.index('a')]
+    subject = inp[inp.index('a')+1:]
+    listOfTable = eval(table)
+    #print(lt,subject)
+    return str(kunit.add(listOfTable,subject)).replace(" ","")
+    #return "still alive"
+
+
 if __name__ == "__main__":
     app.run(debug = True,host="0.0.0.0")
