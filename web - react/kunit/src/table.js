5400d10e3bad177cc20bf292c6aef0cd6fe3f565
Banktts <31396863+Banktts@users.noreply.github.com>
2018-04-03 23:18:40 +0700

    fix table

--- web - react/kunit/src/table.js
+++ web - react/kunit/src/table.js
@@ -10,20 +10,6 @@ class Table extends Component {
     
     const columns = [{
       dataField: 'major',
-      text: 'Program'
-    },{
-      dataField: 'subimport React, { Component } from 'react';
-import BootstrapTable from 'react-bootstrap-table-next'
-import 'bootstrap/dist/css/bootstrap.css';
-require('react-bootstrap-table-next/dist/react-bootstrap-table2.min.css');
-
-class Table extends Component {
-
-  render()
-  { 
-    
-    const columns = [{
-      dataField: 'major',
       text: 'All Subjects'
     },{
       dataField: 'subject',
@@ -56,52 +42,6 @@ class Table extends Component {
       </h3>
       <br/>  
       <BootstrapTable keyField="id" isKey={true} data={eval(this.props.programTable)} columns={ columns1 } tdStyle={ { whiteSpace: 'normal'}}/>
-      
-      </div>
-    );
-  }
-}
-
-export default Table;
-ject',
-      text: 'Subject'
-    },{
-      dataField: 'credit',
-      text: 'Credit'
-    },{
-      dataField: 'by',
-      text: 'From'
-    }]
-
-    const columns1 = [{
-      dataField: '1',
-      text: 'Wellness'
-    },{
-      dataField: '2',
-      text: 'Entrepreneursship'
-    },{
-      dataField: '3',
-      text: 'Thai Citizen and Global Citizen'
-    },{
-      dataField: '4',
-      text: 'Language and Communication'
-    },{
-      dataField: '5',
-      text: 'Aesthetics'
-    }]
-    return (
-      <div>
-      <h3>
-        Subject you should
-      </h3>  
-      <br/>
-      <BootstrapTable keyField="id" isKey={true} data={eval(this.props.table) } columns={ columns } tdStyle={ { whiteSpace: 'normal'}}/>
-      <br/>
-      <h3>
-        Sum credit
-      </h3>
-      <br/>  
-      <BootstrapTable keyField="id" isKey={true} data={eval(this.props.programTable) } columns={ columns1 } tdStyle={ { whiteSpace: 'normal'}}/>
       
       </div>
     );
