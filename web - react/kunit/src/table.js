3c5d2ef0a0984ebf7730eb49cda4eb60288e5288
Banktts <31396863+Banktts@users.noreply.github.com>
2018-04-03 23:18:16 +0700

    Fix table

--- web - react/kunit/src/table.js
+++ web - react/kunit/src/table.js
@@ -12,7 +12,58 @@ class Table extends Component {
       dataField: 'major',
       text: 'Program'
     },{
+      dataField: 'subimport React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next'
+import 'bootstrap/dist/css/bootstrap.css';
+require('react-bootstrap-table-next/dist/react-bootstrap-table2.min.css');
+
+class Table extends Component {
+
+  render()
+  { 
+    
+    const columns = [{
+      dataField: 'major',
+      text: 'All Subjects'
+    },{
       dataField: 'subject',
+      text: 'Subject'
+    },{
+      dataField: 'credit',
+      text: 'Credit'
+    },{
+      dataField: 'by',
+      text: 'From'
+    }]
+
+    const columns1 = [{
+      dataField: "1",
+      text: 'All Subjects'
+    },{
+      dataField: "2",
+      text: 'Credits'
+    }]
+    return (
+      <div>
+      <h3>
+        Subject you should
+      </h3>  
+      <br/>
+      <BootstrapTable keyField="id" isKey={true} data={eval(this.props.table) } columns={ columns } tdStyle={ { whiteSpace: 'normal'}}/>
+      <br/>
+      <h3>
+        Sum credit
+      </h3>
+      <br/>  
+      <BootstrapTable keyField="id" isKey={true} data={eval(this.props.programTable)} columns={ columns1 } tdStyle={ { whiteSpace: 'normal'}}/>
+      
+      </div>
+    );
+  }
+}
+
+export default Table;
+ject',
       text: 'Subject'
     },{
       dataField: 'credit',
