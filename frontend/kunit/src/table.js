f7760c30001e88db20d3f714c54556cec1a118b9
thanabodee thamasaroch <banktts@service.c.semiotic-quasar-193514.internal>
2018-04-11 07:55:41 +0000

    can select, can't get data

--- frontend/kunit/src/table.js
+++ frontend/kunit/src/table.js
@@ -4,10 +4,33 @@ import 'bootstrap/dist/css/bootstrap.css';
 require('react-bootstrap-table-next/dist/react-bootstrap-table2.min.css');
 
 class Table extends Component {
+  constructor(props){
+  	super(props)
+		this.state={
+				selected:"[]"
+		}
+		this.handleSelected=this.handleSelected.bind(this)
+  }
 
+	handleSelected=(e) =>{
+		var selectedBefore=this.state.selected
+			
+		this.setState({selected: selectedBefore.replace("]",",")+e+"]" })
+  
+  }
+  
   render()
   { 
-    
+    const rowEvents = {
+		mode: 'checkbox',	
+		clickToSelect:true,	
+		bgColor:"red"
+	}
+
+		
+	
+	
+
     const columns = [{
       dataField: 'major',
       text: 'All Subjects'
@@ -35,14 +58,16 @@ class Table extends Component {
         Subject you choose	
       </h3>  
       <br/>
-      <BootstrapTable keyField="id" isKey={true} data={eval(this.props.table) } columns={ columns } tdStyle={ { whiteSpace: 'normal'}}/>
+      <BootstrapTable keyField="subject"  selectRow={rowEvents} data={eval(this.props.table) } columns={ columns } tdStyle={ { whiteSpace: 'normal'}}/>
       <br/>
       <h3>
         Sum credit
       </h3>
       <br/>  
       <BootstrapTable keyField="id" isKey={true} data={eval(this.props.programTable)} columns={ columns1 } tdStyle={ { whiteSpace: 'normal'}}/>
-      
+	  <div>
+			 {this.state.selected}
+	  </div>
       </div>
     );
   }
