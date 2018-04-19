392b06c7343a52cba65357e798662db1e970fa68
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-04-19 14:51:53 +0700

    change facebook app id

--- frontend/kunit/src/comment.js
+++ frontend/kunit/src/comment.js
@@ -4,7 +4,8 @@ import FacebookProvider, { Comments } from 'react-facebook';
 export default class Comment extends Component {
   render() {
     return (
-      <FacebookProvider appId="536899426419954">
+      //<FacebookProvider appId="536899426419954">
+      <FacebookProvider appId="2157947631114614">
         <Comments href="https://wisticejent.github.io/KUnit/" num_posts="5" />
       </FacebookProvider>
     );
