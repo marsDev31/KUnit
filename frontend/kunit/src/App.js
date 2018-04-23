b986e60d7b5abbe30e4fc636d47146c3ad264a2c
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-04-23 20:00:27 +0700

    add more information

--- frontend/kunit/src/App.js
+++ frontend/kunit/src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
           <div className="text-box">
           <br/>
           <h3 className="App-text">เคยูนิต(KUnit) เป็นเว็บไซต์ที่ช่วยคำนวณในการลงทะเบียนเรียนวิชาบูรณาการในมหาวิทยาลัยเกษตรศาสตร์ โดยเราสามารถแนะนำคุณได้ว่ามีวิชาอะไรให้คุณเลือกลงทะเบียนบ้าง คุณลงทะเบียนวิชาบูรณาการวิชาใดไปกี่หน่วยกิตแล้ว และที่สำคัญคือคุณเหลืออีกกี่หน่วยกิตที่จำเป็นต้องลงทะเบียนเรียนเพื่อจบการศึกษา</h3>
+          <h3 className="App-text">หากต้องการทราบรายละเอียดอื่นๆของงานสามารถติดตามได้ที่ <a href="https://github.com/WisTiCeJEnT/kunit">Github KUnit</a> </h3>
           </div>
         </header>
         <MajorSearch className = "Major-search" major={this.handleMajor}/>
